57f8ed622cc754f41416610ec8d3ddf9e3faf709
Mohamed Abdel Hameed <mohamed.hameed@3ddx.com>
2024-07-21 09:05:20 +0300

    NF first commit

---
+++ src/main/java/com/hameed/todo/entities/Todo.java
@@ -0,0 +1,27 @@
+package com.hameed.todo.entities;
+
+public class Todo {
+	
+	private Integer id;
+	private String task;
+	private Boolean isDone;
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public Boolean getIsDone() {
+		return isDone;
+	}
+	public void setIsDone(Boolean isDone) {
+		this.isDone = isDone;
+	}
+}
